9141ea12b498310f7804f756ee3333f9cda8067e
boraqorri2021 <bora.qorri@grapecity.com>
2021-12-14 04:26:35 +0100

    CheckBox List

---
+++ source/repos/CheckBoxListApp/wwwroot/JS/JSFile.js
@@ -0,0 +1,27 @@
+﻿export function ChangeMaxRowNumber(RowMaxNr, itemsNr) {
+    if (RowMaxNr == 0) {
+        alert("Please choose a number!");
+    }
+    else {
+        var columnNr = 0;
+        var rowNr = 0;
+        if (RowMaxNr != 0) {
+            if (itemsNr % RowMaxNr == 0) {
+                columnNr = itemsNr / RowMaxNr;
+            }
+            else {
+                columnNr = (itemsNr / RowMaxNr) + 1;
+            }
+        }
+        var column = '';
+        for (var i = 0; i < columnNr; i++) {
+            column += "auto ";
+        }
+        for (var i = 0; i < RowMaxNr; i++) {
+            rowNr += "auto ";
+        }
+        document.getElementById('gridID').style.gridTemplateRows = rowNr;
+        document.getElementById('gridID').style.gridTemplateColumns = column;
+    }
+   
+}
